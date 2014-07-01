3f544ef2ed622f01f9eacaff219433f8ebefdad0
sodas tsai <sodas2002@gmail.com>
2014-07-01 15:49:21 +0800

    Update gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,9 @@
-/*jshint node:true*/
-/*global module:false*/
-
 var localization = require('./localization.js');
 
 module.exports = function(grunt) {
     'use strict';
 
+    //noinspection JSUnresolvedFunction
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
@@ -15,44 +13,96 @@ module.exports = function(grunt) {
                     stdout: true
                 },
                 command: 'node app.js'
+            },
+            deploy: {
+                options: {
+                    stdout: true
+                },
+                command: 'git add --all . && git commit -am "Deploy" && git pull && git push'
             }
         },
 
         less: {
-            default: {
+            options: {
+                ieCompat: false
+            },
+            prod: {
                 options: {
                     compress: true,
-                    cleancss: true,
-                    ieCompat: false
+                    cleancss: true
                 },
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'static/stylesheets',
-                        src: ['**/*.less', '!common/**/*.less'],
-                        dest: 'static/stylesheets',
-                        ext: '.css'
+                files: [{
+                    expand: true,
+                    cwd: 'static/stylesheets',
+                    src: ['**/*.less', '!lib/**/*.less', '!common/**/*.less'],
+                    dest: 'static/stylesheets',
+                    ext: '.css'
+                }]
+            },
+            dev: {
+                files: [{
+                    expand: true,
+                    cwd: 'static/stylesheets',
+                    src: ['**/*.less', '!lib/**/*.less', '!common/**/*.less'],
+                    dest: 'static/stylesheets',
+                    ext: '.css'
+                }]
+            }
+        },
+
+        jshint: {
+            options: {
+                jshintrc: true
+            },
+            frontend: {
+                files: [{
+                    expand: true,
+                    cwd: 'static/scripts',
+                    src: ['**/*.js', '!**/*.min.js', '!lib/**/*.js', '!common/**/*.js']
+                }]
+            }
+        },
+
+        uglify: {
+            options: {
+                banner: '/* Copyright (c) 2014 Wantoto Inc. All rights reserved. */\n'
+            },
+            fontend: {
+                options: {
+                    compress: {
+                        drop_console: true,
+                        dead_code: true,
+                        properties: true,
+                        drop_debugger: true,
+                        conditionals: true,
+                        evaluate: true,
+                        loops: true,
+                        unused: true,
+                        if_return: true
                     }
-                ]
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'static/scripts',
+                    src: ['**/*.js', '!**/*.min.js', '!common/**/*.js', '!lib/**/*.js'],
+                    dest: 'static/scripts',
+                    ext: '.min.js'
+                }]
             }
         },
 
         jade: {
+            options: {
+                data: {
+                    _t: localization.getLocalText('en')
+                }
+            },
             default: {
-                options: {
-                    data: {
-                        _t: localization.getLocalText('en')
-                    }
-                },
-                files: [
-                    {
-                        expand: true,
-                        cwd: '.',
-                        src: ['**/*.jade', '!common/**/*.jade'],
-                        dest: '.',
-                        ext: '.html'
-                    }
-                ]
+                expand: true,
+                cwd: '.',
+                src: ['**/*.jade', '!common/**/*.jade', '!tw/**/*.jade'],
+                dest: '.',
+                ext: '.html'
             },
             tw: {
                 options: {
@@ -60,39 +110,69 @@ module.exports = function(grunt) {
                         _t: localization.getLocalText('tw')
                     }
                 },
-                files: [
-                    {
-                        expand: true,
-                        cwd: '.',
-                        src: ['**/*.jade', '!common/**/*.jade'],
-                        dest: 'tw/',
-                        ext: '.html'
-                    }
-                ]
+                files: [{
+                    expand: true,
+                    cwd: 'tw',
+                    src: ['**/*.jade', '!common/**/*.jade'],
+                    dest: 'tw',
+                    ext: '.html'
+                }]
             }
         },
 
         watch: {
             less: {
-                files: ['static/stylesheets/**/*.less'],
-                tasks: ['less']
+                files: '/static/stylesheets/**/*.less',
+                tasks: ['less:dev']
+            },
+            jade: {
+                files: '**/*.jade',
+                tasks: ['jade']
+            },
+            jshint: {
+                files: ['**/*.js', '!**/*.min.js'],
+                tasks: ['jshint']
+            },
+            uglify: {
+                files: ['static/**/*.js', '!**/*.min.js'],
+                tasks: ['uglify']
             },
             livereload: {
-                files: ['**/*.jade', 'locales/*.json', 'static/**/*.*', '!static/**/*.css'],
+                // Changes of less should be delegated to changes of css.
+                files: ['**/*.html', 'static/**.*', '!static/**/*.less'],
                 options: {
                     livereload: true
+                }
+            }
+        },
+
+        concurrent: {
+            'dev': {
+                tasks: ['watch:less', 'watch:livereload', 'dev-server'],
+                options: {
+                    logConcurrentOutput: true
                 }
             }
         }
     });
 
+    // Register modules
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jade');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-jade');
     grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-concurrent');
 
-    // Tasks
-    grunt.registerTask('build', ['jade', 'less']);
+    // Declare task alias
     grunt.registerTask('dev-server', ['shell:launchDevServer']);
+    grunt.registerTask('dev', ['concurrent:dev']);
+
+    grunt.registerTask('build:js', ['jshint', 'uglify']);
+    grunt.registerTask('build', ['build:js', 'less:prod', 'jade']);
+
+    grunt.registerTask('deploy', ['build', 'shell:deploy']);
+
     grunt.registerTask('default', ['build']);
 };
--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-/* jshint node: true */
-
 var express = require('express');
 var fs = require('fs');
 var jade = require('jade');
@@ -8,49 +6,56 @@ var localization = require('./localization.js');
 
 var app = express();
 
+//noinspection JSUnresolvedFunction
 app.get(/(.*)/, function(req, res) {
     'use strict';
 
-    // Generate Path
     var requestPath = req.params[0];
-    var filePath = __dirname + requestPath;
+
+    // Add index.html if necessary
+    //noinspection JSUnresolvedVariable
+    var filePath = path.join(__dirname, requestPath);
+    //noinspection JSUnresolvedFunction
     if (fs.existsSync(filePath) && fs.lstatSync(filePath).isDirectory()) {
         filePath = path.join(filePath, 'index.html');
     }
-    filePath = filePath.replace(/\.html$/, '.jade');
 
     // Check file existence
-    var fileExists = fs.existsSync(filePath);
+    var jadeFilePath = filePath.replace(/\.html$/, '.jade');
+    //noinspection JSUnresolvedFunction
+    var fileExists = (fs.existsSync(jadeFilePath) && (filePath = jadeFilePath)) || fs.existsSync(filePath);
     if (!fileExists) {
-        console.log('Not Found  %s', requestPath);
+        console.log('Local-Dev: Not Found  %s', requestPath);
         res.status(404).send('404 File Not Found.');
         return;
     }
 
-    // Sned file
-    if (requestPath.indexOf('/static') === 0) {
-        console.log('Load       %s', requestPath);
+    if (!filePath.match(/\.jade$/)) {
+        // Send file directly
+        console.log('Local-Dev: Load       %s', requestPath);
+        //noinspection JSCheckFunctionSignatures
         res.sendfile(filePath);
     } else {
-        // Find locale file
-        var locale = req.query.locale ? req.query.locale : 'en';
-        var _t = localization.getLocalText(locale);
-        if (!_t) {
-            console.log('Error      %s', requestPath);
-            res.status(500).send('500 No such locale');
-            return;
-        }
-
-        // Read and compile Jade file
-        fs.readFile(filePath, 'utf8', function(err, data) {
-            if (!err) {
-                console.log('Render     %s', requestPath);
-
-                var locals = {_t: _t};
-                res.send(jade.compile(data, {pretty: true, filename: filePath})(locals));
-            } else {
-                console.log('Error      %s', requestPath);
+        // Render by jade template engine
+        fs.readFile(filePath, 'utf8', function (err, template) {
+            if (err) {
+                console.log('Local-Dev: Error      %s', requestPath);
                 res.status(500).send('500 Error: %s', err);
+            } else {
+                // Find locale
+                var locale = req.query.locale ? req.query.locale : 'en';
+                var _t = localization.getLocalText(locale);
+                if (!_t) {
+                    console.log('Error      %s', requestPath);
+                    res.status(500).send('500 No such locale');
+                    return;
+                }
+
+                console.log('Local-Dev: Render     %s', requestPath);
+                var locals = {
+                    _t: _t
+                };
+                res.send(jade.compile(template, {pretty: true, filename: filePath})(locals));
             }
         });
     }
@@ -58,6 +63,6 @@ app.get(/(.*)/, function(req, res) {
 
 app.listen(8000, '127.0.0.1', function() {
     'use strict';
-    //noinspection JSUnresolvedVariable
+    //noinspection JSUnresolvedVariable,JSUnresolvedFunction
     console.log('Listening %s on port %d', this.address().address, this.address().port);
 });
